d0240946bedc75730008df814af329dc89220cc5
Tin922 <tinjuric92@gmail.com>
2024-05-03 16:09:28 +0200

    added swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('WorryNot')
+    .setDescription('WorryNot API')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
