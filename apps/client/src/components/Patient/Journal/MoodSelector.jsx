4adb105d7c6c3c5980f0b634402eb5509a08b36a
lukasmiljic <smiljicluka13@gmail.com>
2024-05-09 14:31:27 +0200

    Add appointment component

--- apps/client/src/components/Patient/Journal/MoodSelector.jsx
+++ apps/client/src/components/Patient/Journal/MoodSelector.jsx
@@ -6,17 +6,59 @@ import mad from "../../../../public/moods/mad.svg";
 import neutral from "../../../../public/moods/neutral.svg";
 import sad from "../../../../public/moods/sad.svg";
 import tired from "../../../../public/moods/tired.svg";
+import { useState } from "react";
 
 const MoodSelector = () => {
+  const [currentMood, setCurrentMood] = useState("");
+  const handleMoodChange = (mood) => {
+    console.log(mood);
+    setCurrentMood(mood);
+  };
+
   return (
     <div className={styles.moodSelector}>
-      <img className={styles.mood} src={neutral} alt="neutral" />
-      <img className={styles.mood} src={happy} alt="happy" />
-      <img className={styles.mood} src={down} alt="down" />
-      <img className={styles.mood} src={sad} alt="sad" />
-      <img className={styles.mood} src={confused} alt="confused" />
-      <img className={styles.mood} src={mad} alt="mad" />
-      <img className={styles.mood} src={tired} alt="tired" />
+      <img
+        className={`${styles.mood} ${currentMood === "neutral" ? styles.currentMood : ""}`}
+        src={neutral}
+        alt="neutral"
+        onClick={() => handleMoodChange("neutral")}
+      />
+      <img
+        className={`${styles.mood} ${currentMood === "happy" ? styles.currentMood : ""}`}
+        src={happy}
+        alt="happy"
+        onClick={() => handleMoodChange("happy")}
+      />
+      <img
+        className={`${styles.mood} ${currentMood === "down" ? styles.currentMood : ""}`}
+        src={down}
+        alt="down"
+        onClick={() => handleMoodChange("down")}
+      />
+      <img
+        className={`${styles.mood} ${currentMood === "sad" ? styles.currentMood : ""}`}
+        src={sad}
+        alt="sad"
+        onClick={() => handleMoodChange("sad")}
+      />
+      <img
+        className={`${styles.mood} ${currentMood === "confused" ? styles.currentMood : ""}`}
+        src={confused}
+        alt="confused"
+        onClick={() => handleMoodChange("confused")}
+      />
+      <img
+        className={`${styles.mood} ${currentMood === "mad" ? styles.currentMood : ""}`}
+        src={mad}
+        alt="mad"
+        onClick={() => handleMoodChange("mad")}
+      />
+      <img
+        className={`${styles.mood} ${currentMood === "tired" ? styles.currentMood : ""}`}
+        src={tired}
+        alt="tired"
+        onClick={() => handleMoodChange("tired")}
+      />
     </div>
   );
 };
--- apps/client/src/pages/Patient/HomePage/PatientHomePage.jsx
+++ apps/client/src/pages/Patient/HomePage/PatientHomePage.jsx
@@ -1,11 +1,26 @@
 import Navbar from "../../../components/Navbar/Navbar";
+import JournalArea from "../../../components/Patient/Journal/JournalArea";
 import styles from "./PatientHomePage.module.css";
+import "../../page.css";
+import UpcomingCheckups from "../../../components/Patient/Checkups/UpcomingCheckups";
 
 const PatientHomePage = () => {
   return (
-    <div className={styles.mainArea}>
-      <h1>Patient Home Page</h1>
-      <Navbar currentPage="home"/>
+    <div className="mainArea">
+      <div className={styles.welcomeTextArea}>
+        <h2 className={styles.pageSubtitle}>Pozdrav Marta!</h2>
+        <h1 className={styles.pageTitle}>Započni svoj dan</h1>
+      </div>
+
+      <JournalArea />
+      <UpcomingCheckups />
+
+      <div className="challangesArea"></div>
+
+      <div className="habitsArea"></div>
+
+      <div className="sosButtonArea"></div>
+      <Navbar currentPage="home" />
     </div>
   );
 };
