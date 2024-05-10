076f834ef9c90e8e155c854250b20835fbb84425
Tin922 <tinjuric92@gmail.com>
2024-05-10 20:33:43 +0200

    added PatientRequest component

--- apps/client/src/pages/Doctor/HomePage/DoctorHomePageRequests.jsx
+++ apps/client/src/pages/Doctor/HomePage/DoctorHomePageRequests.jsx
@@ -1,33 +1,53 @@
 import styles from "./DoctorHomePageRequests.module.css";
-
+import PatientRequest from "../../../components/DoctorPatient/PatientRequest/PatientRequest";
+const patients = [
+  {
+    id: 1,
+    name: "Marta",
+    surname: "Divić",
+    date: "2024-13-05",
+    profilePhoto: "/patients/profilePhoto1.svg",
+  },
+  {
+    id: 2,
+    name: "Jakov",
+    surname: "Matić",
+    date: "2024-13-05",
+    profilePhoto: "/patients/profilePhoto2.svg",
+  },
+];
 const DoctorHomePageRequests = () => {
   return (
     <>
-      <div className={styles.buttonWrapper}>
-        <button className={styles.activeButton}>Zahtjevi</button>
-        <button className={styles.inactiveButton}>Klijenti</button>
+      <div className={styles.buttonWrapperOuterContianer}>
+        <div className={styles.buttonWrapper}>
+          <button className={styles.activeButton}>Zahtjevi</button>
+          <button className={styles.inactiveButton}>Klijenti</button>
+        </div>
       </div>
 
       <div>
         <div>
           <div className={styles.sortOptions}>
-            <img src="/doctorHomePage/1" alt="" />
-            <p>Pacijetni</p>
+            <div>
+              <img src="/doctorHomePage/1.svg" alt="" />
+              <p>Pacijetni</p>
+            </div>
+            <div>
+              <img src="/doctorHomePage/2.svg" alt="" />
+              <p>Neupisani</p>
+            </div>
+            <div>
+              <img src="/doctorHomePage/3.svg" alt="" />
+              <p>Najnoviji</p>
+            </div>
+            <div>
+              <img src="/doctorHomePage/4.svg" alt="" />
+              <p>Najstariji</p>
+            </div>
           </div>
-          <div>
-            <img src="/doctorHomePage/2" alt="" />
-            <p>Neupisani</p>
-          </div>
-          <div>
-            <img src="/doctorHomePage/3" alt="" />
-            <p>Najnoviji</p>
-          </div>
-          <div>
-            <img src="/doctorHomePage/4" alt="" />
-            <p>Najstariji</p>
-          </div>
+          <PatientRequest patients={patients} />
         </div>
-        <div></div>
       </div>
     </>
   );
